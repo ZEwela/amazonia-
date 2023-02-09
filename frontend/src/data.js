a8905c022978ee05d0d52d0b865fbba9243e0554
Ewelina <ewelinazawol@gmail.com>
2023-02-09 16:53:55 +0000

    Render products

---
+++ frontend/src/data.js
@@ -0,0 +1,40 @@
+export default {
+    products: [
+        {
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image:'/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image:'/images/d1.jpg',
+            price: 55,
+            brand: 'Adidas',
+            rating: 4.7,
+            numReviews: 11
+        },
+        {
+            name: 'Fit Pants',
+            category: 'Pants',
+            image:'/images/d1.jpg',
+            price: 45,
+            brand: 'Nike',
+            rating: 0,
+            numReviews: 0
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image:'/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.8,
+            numReviews: 8
+        },
+    ]
+}
